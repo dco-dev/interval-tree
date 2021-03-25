c8f8b75fccf6a7012df3944bf6bc1b56e692dc88
Dan Lentz <dan.lentz@dean.com>
2021-03-25 10:24:09 -0400

    +clojure.math.combinatorics

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
 
   :plugins [[lein-asciidoctor  "0.1.14"]]
 
